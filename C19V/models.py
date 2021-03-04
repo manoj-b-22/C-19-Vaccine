6fa8047c4a332dcc3a7703e43dcced099ec55345
manoj-b-22 <manojsrinivas.b19@iiits.in>
2021-03-04 22:06:25 +0530

    added Person model

--- C19V/models.py
+++ C19V/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.utils import timezone
+from phone_field import PhoneField 
 # Create your models here.
+
+class VaccinatedPerson(models.Model):
+    name = models.CharField(verbose_name='Name',max_length=100,help_text=('Name of the Vaccinated Person'))
+    dob = models.DateField(verbose_name='Date of Birth')
+    phone_no = PhoneField(help_text=("Contact phone number"))
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def age(self):
+        return timezone.now().year - self.dob.year
--- C19Vaccine/settings.py
+++ C19Vaccine/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'C19V.apps.C19VConfig'
+    'C19V.apps.C19VConfig',
+    'phone_field',
 ]
 
 MIDDLEWARE = [
