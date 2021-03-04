6949bc62a34f30394eab1eb4bc2b25876ef7fe74
manoj-b-22 <manojsrinivas.b19@iiits.in>
2021-03-04 22:29:33 +0530

    choices added to a model

--- C19V/models.py
+++ C19V/models.py
@@ -4,8 +4,10 @@ from phone_field import PhoneField
 # Create your models here.
 
 class VaccinatedPerson(models.Model):
-    name = models.CharField(verbose_name='Name',max_length=100,help_text=('Name of the Vaccinated Person'))
+
     GENDER_CHOICES = (('M','Male'),('F','Female'))
+
+    name = models.CharField(verbose_name='Name',max_length=100,help_text=('Name of the Vaccinated Person'))
     gender = models.CharField(max_length=1,choices=GENDER_CHOICES)
     dob = models.DateField(verbose_name='Date of Birth')
     phone_no = PhoneField(help_text=("Contact phone number"))
@@ -14,3 +16,6 @@ class VaccinatedPerson(models.Model):
     @property
     def age(self):
         return timezone.now().year - self.dob.year
+
+    def __str__(self):
+        return self.name    
