0ae1a9c9e17592a17e94488bd58cb309b63ac7a7
manoj-b-22 <manojsrinivas.b19@iiits.in>
2021-03-06 10:15:24 +0530

    changed urls of statistics

--- C19V/urls.py
+++ C19V/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('HealthReport',views.health,name="health"),
-    path('Statistics',views.stats,name='stats'),
-    path('create_person',views.createPerson,name="create_person"),
+    path('HealthReport/',views.health,name="health"),
+    path('Statistics/',views.stats,name='stats'),
+    path('create_person/',views.createPerson,name="create_person"),
 ]
