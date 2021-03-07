6fb14960ffc11becd93801bf477a7917a033bdfa
Kranthi-L <kranthikusal.l19@iiits.in>
2021-03-07 12:58:01 +0530

    edited vc register and login page

--- C19V/urls.py
+++ C19V/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:pk>/', views.home, name="home"),
-    path('HealthReport/<str:pk>/', views.health, name="health"),
-    path('Statistics/<str:pk>/', views.stats, name='stats'),
-    path('VaccinationCentre/<str:pk>/', views.dashboard, name="dashboard"),
-    path('VC/Report/<str:pk>/', views.report, name="report"),
-    path('StatisticsVC/<str:pk>/', views.statsVC, name='statsVC'),
-    path('Login/', views.Login, name="login"),
-    path('LoginVC/', views.LoginVC, name="loginvc"),
+    path('home/<str:pk>/', views.home, name="home"),
+    path('healthreport/<str:pk>/', views.health, name="health"),
+    path('statistics/<str:pk>/', views.stats, name='stats'),
+    path('vc/home/<str:pk>/', views.dashboard, name="dashboard"),
+    path('vc/report/<str:pk>/', views.report, name="report"),
+    path('vc/statistics/<str:pk>/', views.statsVC, name='statsVC'),
+    path('login/', views.Login, name="login"),
+    path('vc/login/', views.LoginVC, name="loginvc"),
     path('create_person/', views.createPerson, name="create_person"),
-    path('RegisterVC/',views.registerVC,name='RegisterVC'),
+    path('vc/register/', views.registerVC, name='registervc'),
 ]
