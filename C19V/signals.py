1913041c860cc341a105f491e3b294cc6b5e98f3
manoj-b-22 <manojsrinivas.b19@iiits.in>
2021-03-22 10:35:37 +0530

    added signals

---
+++ C19V/signals.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from . import models
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created and not User.is_staff:
+        models.VaccinatedPerson.objects.create(user=instance)
+    if created and  User.is_staff:
+        models.TestCentre.objects.create(user=instance)    
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
